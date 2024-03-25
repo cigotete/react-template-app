bdbddd8c216f1e07f6ec43b0ec28675f9dee70b2
Camilo Figueroa <cigotete@gmail.com>
2024-03-25 17:35:57 -0500

    First commit. Base React project.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello world!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
